da0cc7126cbf3b67de7d791529e83f7eb699387c
Ender Bakır <enderyx_123@hotmail.com>
2024-03-04 19:44:17 +0300

    OrderCreatedEventConsumer completed

--- Shared/RabbitMQSettingsConst.cs
+++ Shared/RabbitMQSettingsConst.cs
@@ -9,5 +9,7 @@ namespace Shared
     public class RabbitMQSettingsConst
     {
         public const string StockOrderCreatedEventQueueName = "stock-order-created-queue";
+
+        public const string StockReservedEventQueueName = "stock-reserved-queue";
     }
 }
--- Shared/StockNotReservedEvent.cs
+++ Shared/StockNotReservedEvent.cs
@@ -10,8 +10,6 @@ namespace Shared
     {
         public int OrderId { get; set; }
 
-        public string BuyerId { get; set; }
-
         public string Message { get; set; }
     }
 }
