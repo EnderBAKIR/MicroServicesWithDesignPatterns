3c25df3a4fbfb64d78a0f8d4938884f0d88a700a
Ender Bakır <enderyx_123@hotmail.com>
2024-03-05 20:50:41 +0300

    Database connection made for WorkerService and Order.API => Docker/SqlServer 2022 ubuntu

--- SagaStateMachineWorkerService/Models/OrderStateInstance.cs
+++ SagaStateMachineWorkerService/Models/OrderStateInstance.cs
@@ -1,6 +1,7 @@
 ﻿using MassTransit;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,7 @@ namespace SagaStateMachineWorkerService.Models
 
         public string CVV { get; set; }
 
+        [Column(TypeName ="decimal(18 ,2)")]
         public decimal TotalPrice { get; set; }
 
         public DateTime CreatedDate { get; set; }
