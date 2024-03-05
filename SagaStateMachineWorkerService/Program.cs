d58fa57a623e27ae8b3c1a47ae79344e1dbf160d
Ender Bakır <enderyx_123@hotmail.com>
2024-03-05 20:16:58 +0300

    MassTransit RabbitMQ configuration

--- SagaStateMachineWorkerService/Program.cs
+++ SagaStateMachineWorkerService/Program.cs
@@ -1,7 +1,51 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using SagaStateMachineWorkerService;
+using SagaStateMachineWorkerService.Models;
+using Shared;
+using System.Reflection;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddMassTransit(async cfg =>
+{
+    cfg.AddSagaStateMachine<OrderStateMachine, OrderStateInstance>().EntityFrameworkRepository(opt =>
+    {
+        opt.AddDbContext<DbContext, OrderStateDbContext>((provider, options) =>
+        {
+
+            options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), m =>
+            {
+                m.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+            });
+
+
+
+        });
+    });
+
+    cfg.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(configure =>
+    {
+        configure.Host(builder.Configuration.GetConnectionString("RabbitMQ"));
+
+
+
+        configure.ReceiveEndpoint(RabbitMQSettingsConst.OrderSaga, e =>
+        {
+            e.ConfigureSaga<OrderStateInstance>(provider);
+        });
+    }));
+
+
+});
+
+
+
+
+
+
+
 
 var host = builder.Build();
 host.Run();
