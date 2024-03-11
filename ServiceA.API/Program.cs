9ac6ec86b49bcbeb689c7931fe2e531822e65dab
Ender Bakır <enderyx_123@hotmail.com>
2024-03-11 13:42:51 +0300

    Added GetCircuitBreakerPolicy In ServiceA's Program.cs

--- ServiceA.API/Program.cs
+++ ServiceA.API/Program.cs
@@ -16,14 +16,40 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<ProductService>(opt =>
 {
     opt.BaseAddress = new Uri("https://localhost:7296/api/products/");
-}).AddPolicyHandler(GetRetryPolicy());
+}).AddPolicyHandler(GetCircuitBreakerPolicy());
 
 
 
 
 var app = builder.Build();
 
+IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions.HandleTransientHttpError().OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound).WaitAndRetryAsync(5, retryAttempt =>
+    {
+        Debug.WriteLine($"Retry Count {retryAttempt}:");
+        return TimeSpan.FromSeconds(10);
+    }, onRetryAsync: onretryAsync);
+}
+Task onretryAsync(DelegateResult<HttpResponseMessage> arg1, TimeSpan arg2)
+{
+    Debug.WriteLine($"Request is made again: {arg2.TotalMilliseconds} ");
+    return Task.CompletedTask;
+}
 
+IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions.HandleTransientHttpError().CircuitBreakerAsync(3, TimeSpan.FromSeconds(10),onBreak: (arg1, arg2) =>
+    {
+        Debug.WriteLine("Circiut Breaker => On Break");
+    }, onReset: () =>
+    {
+        Debug.WriteLine("Circiut Breaker => On Reset");
+    }, onHalfOpen: () =>
+    {
+        Debug.WriteLine("Circiut Breaker => On HalfOpen");
+    });
+}
 
 
 // Configure the HTTP request pipeline.
@@ -40,16 +66,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
-{
-    return HttpPolicyExtensions.HandleTransientHttpError().OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound).WaitAndRetryAsync(5, retryAttempt =>
-    {
-        Debug.WriteLine($"Retry Count {retryAttempt}:");
-        return TimeSpan.FromSeconds(10);
-    }, onRetryAsync: onretryAsync);
-}
-Task onretryAsync(DelegateResult<HttpResponseMessage> arg1, TimeSpan arg2)
-{
-    Debug.WriteLine($"Request is made again: {arg2.TotalMilliseconds} ");
-    return Task.CompletedTask;
-}
+
+
