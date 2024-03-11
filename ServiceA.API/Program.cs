5883865b836db8714da2d032fa3bab1f8e72d70c
Ender Bakır <enderyx_123@hotmail.com>
2024-03-11 12:59:25 +0300

    Added RetryPolicy In Program.cs For ServiceA.API

--- ServiceA.API/Program.cs
+++ ServiceA.API/Program.cs
@@ -1,4 +1,9 @@
+using Polly;
+using Polly.Extensions.Http;
 using ServiceA.API;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.CompilerServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +16,15 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<ProductService>(opt =>
 {
     opt.BaseAddress = new Uri("https://localhost:7296/api/products/");
-});
+}).AddPolicyHandler(GetRetryPolicy());
+
 
 
 
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,3 +40,16 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions.HandleTransientHttpError().OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound).WaitAndRetryAsync(5, retryAttempt =>
+    {
+        Debug.WriteLine($"Retry Count {retryAttempt}:");
+        return TimeSpan.FromSeconds(10);
+    }, onRetryAsync: onretryAsync);
+}
+Task onretryAsync(DelegateResult<HttpResponseMessage> arg1, TimeSpan arg2)
+{
+    Debug.WriteLine($"Request is made again: {arg2.TotalMilliseconds} ");
+    return Task.CompletedTask;
+}
